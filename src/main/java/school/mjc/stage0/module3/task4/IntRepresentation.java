7e88a5451b082e3b04e124c20e7a09e664ac0a68
Iurii Cherkasov <cherkasov1010@gmail.com>
2023-07-30 19:17:04 +0300

    M3 t4

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c, h, a, r;
+        c = 99;
+        h = 104;
+        a = 97;
+        r = 114;
+        System.out.println((int)c);
+        System.out.println((int)h);
+        System.out.println((int)a);
+        System.out.println((int)r);
     }
 }
